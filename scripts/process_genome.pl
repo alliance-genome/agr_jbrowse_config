3e47153438dada8c2596dd9f3dba99606e038721
Scott Cain <scott+git@scottcain.net>
2017-03-06 20:33:13 +0000

    placeholder files for scripts to come

---
+++ scripts/process_genome.pl
@@ -0,0 +1,5 @@
+#this script will take fasta and GFF as input and process them into data for JBRowse, 
+#including:
+#    tracks/All Genes/ track files
+#    seq/ from fasta
+#    names/ index files
---
+++ scripts/upload_to_S3.pl
@@ -0,0 +1,3 @@
+#this will take a JBrowse data directory containing tracks/, seq/, and names/
+#and upload them to S3 using the aws command line tool.  Note that the keys
+#required for accessing S3 on the command line must be stored in ~/.aws/credentials.
