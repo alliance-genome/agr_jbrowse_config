088d8e02af4ce3a4215a8134cffff91c296ad630
Scott Cain <scott+git@scottcain.net>
2017-04-21 14:13:07 +0000

    creating an index file, set website and adding a CORS option

--- scripts/upload_to_S3.pl
+++ scripts/upload_to_S3.pl
@@ -11,7 +11,9 @@ upload_to_S3.pl - Uploads a set of JBrowse track data to an AWS S3 bucket
 =head1 SYNOPSYS
 
   % upload_to_S3.pl --aws <path> --bucket <name> \
-                    --local <path> --remote <path> [--notcompressed]
+                    --local <path> --remote <path> \
+                   [--notcompressed]
+                   [--cors]
 
 =head1 AUTHOR
 
@@ -34,14 +36,15 @@ it under the same terms as Perl itself.
 #   * path to local track data
 #   * path in bucket (make a resonable default?)
 
-my ($AWS, $BUCKET, $LOCAL, $REMOTE, $NOTCOMPRESSED);
+my ($AWS, $BUCKET, $LOCAL, $REMOTE, $NOTCOMPRESSED, $CORS);
 
 GetOptions(
     'aws=s'         => \$AWS,
     'bucket=s'      => \$BUCKET,
     'local=s'       => \$LOCAL,
     'remote=s'      => \$REMOTE,
-    'notcompressed' => \$NOTCOMPRESSED
+    'notcompressed' => \$NOTCOMPRESSED,
+    'cors'          => \$CORS
 ) or ( system( 'pod2text', $0 ), exit -1 );
 
 $AWS    ||= '/home/scain/scain/bin/aws';
@@ -69,5 +72,26 @@ system("$AWS s3 cp --acl public-read names/meta.json $REMOTEPATH/names/meta.json
 #transfer seq
 system("$AWS s3 cp --recursive --acl public-read seq/ $REMOTEPATH/seq/");
 
+#create bogus index.html, set website and optionally CORS
+open(INDEX, ">", "/tmp/index.html") or die;
+while(<DATA>) {
+    print;
+}
+close INDEX;
+
+system("$AWS s3 cp --acl public-read /tmp/index.html s3://$BUCKET");
+system("$AWS s3 website s3://$BUCKET --index-document index.html");
+
+if ($CORS) {
+    system("$AWS s3api put-bucket-cors --bucket $BUCKET --cors-configuration '{\"CORSRules\": [{\"AllowedOrigins\": [\"*\"],\"AllowedHeaders\": [\"*\" ],\"AllowedMethods\": [\"GET\"],\"MaxAgeSeconds\": 3000}]}'");
+}
+
 print "Transfer complete\n";
 exit(0);
+
+
+__END__
+<html>
+This is a simple index file to make S3 web serving work.
+</html>
+
