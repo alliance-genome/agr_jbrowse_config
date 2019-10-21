5840aeca34d20f6459afe694e44a419adf78a798
Nathan Dunn <ndunn@me.com>
2019-10-21 15:51:20 -0700

    updated all

--- jbrowse/data/worm/tracks/variants/process_vcf.sh
+++ jbrowse/data/worm/tracks/variants/process_vcf.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 rm -f VCF_*
-wget  http://download.alliancegenome.org/2.3.0/VCF/WBcel235/VCF_WBcel235_11.vcf
-bgzip -c VCF_WBcel235_11.vcf > VCF_WBcel235_11.vcf.gz
-tabix -p vcf VCF_WBcel235_11.vcf.gz
+wget  http://download.alliancegenome.org/2.3.0/VCF/WBcel235/VCF_WBcel235_14.vcf -o test.vcf
+bgzip -c VCF_WBcel235_14.vcf > VCF_WBcel235_14.vcf.gz
+tabix -p vcf VCF_WBcel235_14.vcf.gz
--- scripts/fetch-vcf.sh
+++ scripts/fetch-vcf.sh
@@ -1,5 +1,10 @@
- wget http://download.alliancegenome.org/2.3.0/VCF/WBcel235/VCF_WBcel235_11.vcf
- wget http://download.alliancegenome.org/2.3.0/VCF/GRCz11/VCF_GRCz11_13.vcf
- wget http://download.alliancegenome.org/2.3.0/VCF/GRCm38/VCF_GRCm38_12.vcf
- wget http://download.alliancegenome.org/2.3.0/VCF/Rnor60/VCF_Rnor60_11.vcf
- wget http://download.alliancegenome.org/2.3.0/VCF/R6/VCF_R6_11.vcf
+#!/bin/sh 
+
+#https://fms.alliancegenome.org/api/datafile/by/VCF?latest=true
+
+
+cd ../jbrowse/data/worm/tracks/variants && rm -f VCF_* && curl http://download.alliancegenome.org/2.3.0/VCF/WBcel235/VCF_WBcel235_14.vcf -o VCF_latest.vcf && bgzip VCF_latest.vcf && tabix VCF_latest.vcf.gz && cd ../../../../../scripts/
+cd ../jbrowse/data/fish/tracks/variants && rm -f VCF_* && curl http://download.alliancegenome.org/2.3.0/VCF/GRCz11/VCF_GRCz11_16.vcf -o VCF_latest.vcf && bgzip VCF_latest.vcf && tabix VCF_latest.vcf.gz && cd ../../../../../scripts/
+cd ../jbrowse/data/MGI/tracks/variants && rm -f VCF_* && curl http://download.alliancegenome.org/2.3.0/VCF/GRCm38/VCF_GRCm38_15.vcf -o VCF_latest.vcf && bgzip VCF_latest.vcf && tabix VCF_latest.vcf.gz && cd ../../../../../scripts/
+cd ../jbrowse/data/RGD/tracks/variants && rm -f VCF_* && curl http://download.alliancegenome.org/2.3.0/VCF/Rnor60/VCF_Rnor60_14.vcf -o VCF_latest.vcf && bgzip VCF_latest.vcf && tabix VCF_latest.vcf.gz && cd ../../../../../scripts/
+cd ../jbrowse/data/fly/tracks/variants && rm -f VCF_* && curl http://download.alliancegenome.org/2.3.0/VCF/R6/VCF_R6_14.vcf -o VCF_latest.vcf && bgzip VCF_latest.vcf && tabix VCF_latest.vcf.gz && cd ../../../../../scripts/
