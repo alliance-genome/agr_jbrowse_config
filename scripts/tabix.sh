9d26370e6e7315068159b2b12a0a86112ab552e1
Nathan Dunn <ndunn@me.com>
2019-10-21 16:16:39 -0700

    removed

--- scripts/tabix.sh
+++
@@ -1,7 +0,0 @@
-
-bgzip VCF_WBcel235_11.vcf; tabix VCF_WBcel235_11.vcf.gz
-bgzip VCF_GRCz11_13.vcf; tabix VCF_GRCz11_13.vcf.gz
-bgzip VCF_GRCm38_12.vcf; tabix VCF_GRCm38_12.vcf.gz
-bgzip VCF_Rnor60_11.vcf; tabix VCF_Rnor60_11.vcf.gz
-bgzip VCF_R6_11.vcf; tabix VCF_R6_11.vcf.gz
-
