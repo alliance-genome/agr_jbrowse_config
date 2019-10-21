c562e05bd3c4b459eef7625ac173d71fa218a16c
Nathan Dunn <ndunn@me.com>
2019-10-21 16:20:18 -0700

    removed script

--- jbrowse/data/RGD/tracks/variants/process_vcf.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-rm -f VCF_*
-wget  http://download.alliancegenome.org/2.3.0/VCF/Rnor60/VCF_Rnor60_11.vcf
-bgzip -c VCF_Rnor60_11.vcf > VCF_Rnor60_11.vcf.gz
-tabix -p vcf VCF_Rnor60_11.vcf.gz
--- jbrowse/data/worm/tracks/variants/process_vcf.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-rm -f VCF_*
-wget  http://download.alliancegenome.org/2.3.0/VCF/WBcel235/VCF_WBcel235_14.vcf -o test.vcf
-bgzip -c VCF_WBcel235_14.vcf > VCF_WBcel235_14.vcf.gz
-tabix -p vcf VCF_WBcel235_14.vcf.gz
