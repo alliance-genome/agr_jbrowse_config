d162c91838577ff18b17428df7ea425edf378bd5
Nathan Dunn <ndunn@me.com>
2020-02-13 12:33:56 -0800

    added newer apollo data

--- scripts/fetch_vcf.sh
+++ scripts/fetch_vcf.sh
@@ -6,29 +6,29 @@ ZEBRAFISHVCF="VCF_GRCz11_22.vcf.gz"
 MOUSEVCF="VCF_GRCm38_21.vcf.gz"
 RATVCF="VCF_Rnor60_19.vcf.gz"
 
-rm -rf ../jbrowse/data/worm/jbrowse
-mkdir ../jbrowse/data/worm/jbrowse
-rm -rf ../jbrowse/data/zebrafish/jbrowse
-mkdir  ../jbrowse/data/zebrafish/jbrowse
-rm -rf ../jbrowse/data/fly/jbrowse
-mkdir  ../jbrowse/data/fly/jbrowse
-rm -rf ../jbrowse/data/MGI/jbrowse
-mkdir  ../jbrowse/data/MGI/jbrowse
-rm -rf ../jbrowse/data/RGD/jbrowse
-mkdir  ../jbrowse/data/RGD/jbrowse
+rm -rf ../apollo/data/worm/jbrowse
+mkdir ../apollo/data/worm/jbrowse
+rm -rf ../apollo/data/zebrafish/jbrowse
+mkdir  ../apollo/data/zebrafish/jbrowse
+rm -rf ../apollo/data/fly/jbrowse
+mkdir  ../apollo/data/fly/jbrowse
+rm -rf ../apollo/data/MGI/jbrowse
+mkdir  ../apollo/data/MGI/jbrowse
+rm -rf ../apollo/data/RGD/jbrowse
+mkdir  ../apollo/data/RGD/jbrowse
 
-wget -P ../jbrowse/data/worm/jbrowse/ https://s3.amazonaws.com/agrjbrowse/VCF/$WORMVCF
-wget -P ../jbrowse/data/worm/jbrowse/ https://s3.amazonaws.com/agrjbrowse/VCF/$WORMVCF.tbi
+wget -P ../apollo/data/worm/jbrowse/ https://s3.amazonaws.com/agrjbrowse/VCF/$WORMVCF
+wget -P ../apollo/data/worm/jbrowse/ https://s3.amazonaws.com/agrjbrowse/VCF/$WORMVCF.tbi
 
-wget -P ../jbrowse/data/zebrafish/jbrowse/ https://s3.amazonaws.com/agrjbrowse/VCF/$ZEBRAFISHVCF
-wget -P ../jbrowse/data/zebrafish/jbrowse/ https://s3.amazonaws.com/agrjbrowse/VCF/$ZEBRAFISHVCF.tbi
+wget -P ../apollo/data/zebrafish/jbrowse/ https://s3.amazonaws.com/agrjbrowse/VCF/$ZEBRAFISHVCF
+wget -P ../apollo/data/zebrafish/jbrowse/ https://s3.amazonaws.com/agrjbrowse/VCF/$ZEBRAFISHVCF.tbi
 
-wget -P ../jbrowse/data/fly/jbrowse/ https://s3.amazonaws.com/agrjbrowse/VCF/$FLYVCF
-wget -P ../jbrowse/data/fly/jbrowse/ https://s3.amazonaws.com/agrjbrowse/VCF/$FLYVCF.tbi
+wget -P ../apollo/data/fly/jbrowse/ https://s3.amazonaws.com/agrjbrowse/VCF/$FLYVCF
+wget -P ../apollo/data/fly/jbrowse/ https://s3.amazonaws.com/agrjbrowse/VCF/$FLYVCF.tbi
 
-wget -P ../jbrowse/data/MGI/jbrowse/ https://s3.amazonaws.com/agrjbrowse/VCF/$MOUSEVCF
-wget -P ../jbrowse/data/MGI/jbrowse/ https://s3.amazonaws.com/agrjbrowse/VCF/$MOUSEVCF.tbi
+wget -P ../apollo/data/MGI/jbrowse/ https://s3.amazonaws.com/agrjbrowse/VCF/$MOUSEVCF
+wget -P ../apollo/data/MGI/jbrowse/ https://s3.amazonaws.com/agrjbrowse/VCF/$MOUSEVCF.tbi
 
-wget -P ../jbrowse/data/RGD/jbrowse/ https://s3.amazonaws.com/agrjbrowse/VCF/$RATVCF
-wget -P ../jbrowse/data/RGD/jbrowse/ https://s3.amazonaws.com/agrjbrowse/VCF/$RATVCF.tbi
+wget -P ../apollo/data/RGD/jbrowse/ https://s3.amazonaws.com/agrjbrowse/VCF/$RATVCF
+wget -P ../apollo/data/RGD/jbrowse/ https://s3.amazonaws.com/agrjbrowse/VCF/$RATVCF.tbi
 
