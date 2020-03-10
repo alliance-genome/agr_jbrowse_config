5a25edb018d61909e4a1a74a07ffe7ca6c6f84b9
Nathan Dunn <ndunn@me.com>
2020-03-09 17:15:45 -0700

    updated per scott recommendation

--- scripts/fetch_vcf.sh
+++ scripts/fetch_vcf.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-WORMVCF="VCF_WBcel235_64.vcf.gz"
-ZEBRAFISHVCF="VCF_GRCz11_64.vcf.gz"
-FLYVCF="VCF_R6_64.vcf.gz"
-MOUSEVCF="VCF_GRCm38_64.vcf.gz"
-RATVCF="VCF_Rnor60_53.vcf.gz"
+WORMVCF="VCF_WBcel235_68.vcf.gz"
+ZEBRAFISHVCF="VCF_GRCz11_68.vcf.gz"
+FLYVCF="VCF_R6_68.vcf.gz"
+MOUSEVCF="VCF_GRCm38_68.vcf.gz"
+RATVCF="VCF_Rnor60_56.vcf.gz"
 
 rm -rf ../apollo/data/worm/jbrowse
 mkdir ../apollo/data/worm/jbrowse
