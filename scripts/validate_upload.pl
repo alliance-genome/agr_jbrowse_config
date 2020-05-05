5b44fd12d33be58baf008c879adee274a4bab3f5
Scott Cain <scott+git@scottcain.net>
2020-05-05 00:00:35 +0000

    quite a bit of temp code in here but it correctly fetches the remote md5 sum

--- scripts/validate_upload.pl
+++ scripts/validate_upload.pl
@@ -3,8 +3,9 @@ use strict;
 use warnings;
 
 use Getopt::Long;
+#use URI::Escape;
 
-my ($AWS, $BUCKET, $LOCAL, $REMOTE, $PROFILE,$SKIPSEQ);
+my ($AWS, $BUCKET, $LOCAL, $REMOTE, $PROFILE,$SKIPFILECOUNT,$SKIPSEQ);
 
 GetOptions(
     'aws=s'         => \$AWS,
@@ -12,6 +13,7 @@ GetOptions(
     'local=s'       => \$LOCAL,
     'remote=s'      => \$REMOTE,
     'profile=s'     => \$PROFILE,
+    'skipfilecount' => \$SKIPFILECOUNT,
     'skipseq'       => \$SKIPSEQ
 ) or ( system( 'pod2text', $0 ), exit -1 );
 
@@ -34,29 +36,72 @@ if ($PROFILE) {
 
 my $REMOTEPATH = "s3://$BUCKET/$REMOTE";
 
-chdir($LOCAL) or die "unable to cd to $LOCAL";
+#chdir($LOCAL) or die "unable to cd to $LOCAL";
 
+my @remotetrack_result = `$AWS s3 ls --recursive $REMOTEPATH/tracks |grep -v htaccess`;
+my @remotenames_result = `$AWS s3 ls --recursive $REMOTEPATH/names |grep -v htaccess`;
+
+unless ($SKIPFILECOUNT) {
 #check tracks
-my @localtrack_result = `ls -Rl $LOCAL/tracks |grep -P "^-"|wc -l`;
-my @remotetrack_result = `$AWS s3 ls --recursive $REMOTEPATH/tracks |grep -v htaccess|wc -l`;
-
-if ($localtrack_result[0] != $remotetrack_result[0]) {
-    warn "WARNING: file count differs between local and remote tracks directories\nLOCAL: $localtrack_result[0]\nREMOTE: $remotetrack_result[0]\n";
-}
-else {
-    warn "all is well in tracks\n";
-}
+    my @localtrack_result = `ls -Rl $LOCAL/tracks |grep -P "^-"|wc -l`;
+    my $remote_count = scalar @remotetrack_result;
+    if ($localtrack_result[0] != $remote_count) {
+        warn "WARNING: file count differs between local and remote tracks directories\nLOCAL: $localtrack_result[0]\nREMOTE: $remote_count\n";
+    }
+    else {
+        warn "File counts agree in tracks.\n";
+    }
 
 #check names
-my @localnames_result = `ls -Rl $LOCAL/names |grep -P "^-"|wc -l`;
-my @remotenames_result = `$AWS s3 ls --recursive $REMOTEPATH/names |grep -v htaccess|wc -l`;
+    my @localnames_result = `ls -Rl $LOCAL/names |grep -P "^-"|wc -l`;
 
-if ($localnames_result[0] != $remotenames_result[0]) {
-    warn "WARNING: file count differs between local and remote names directories\nLOCAL: $localnames_result[0]\nREMOTE: $remotenames_result[0]\n";
+    $remote_count = scalar @remotenames_result;
+    if ($localnames_result[0] != $remote_count) {
+        warn "WARNING: file count differs between local and remote names directories\nLOCAL: $localnames_result[0]\nREMOTE: $remote_count\n";
+    }
+    else {
+        warn "File counts agree in names.\n";
+    }
+}
+
+warn "starting md5 comparison...\n";
+
+warn $remotetrack_result[0];
+warn "\n$remotetrack_result[1]\n";
+
+my (undef,undef,undef,$path1,$path2) = split(/\s+/, $remotetrack_result[0]);
+my $path;
+if ($path2) {
+    $path = "$path1%20$path2";
 }
 else {
-    warn "all is well in names\n";
+    $path = $path1;
 }
 
+my $result = get_remote_md5($path);
+warn $result;
 
 exit(0);
+
+
+sub get_remote_md5 {
+    my $path = shift;
+
+    my $url = "https://s3.amazonaws.com/agrjbrowse/$path";
+
+    warn $url;
+
+    my @remote_header = `curl -I $url`;
+
+    my $sum;
+    for my $line (@remote_header) {
+        if ($line =~ /ETag:\s*"(.*)"/) {
+            $sum = $1;
+        } 
+    }
+
+    die "couldn't get md5 sum for $path\n" unless $sum;
+
+    return $sum;
+
+}
