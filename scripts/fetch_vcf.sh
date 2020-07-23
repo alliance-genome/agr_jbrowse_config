27327f683caa3fb1634d6229219ff8485e6d89dd
Nathan Dunn <ndunn@me.com>
2020-07-23 09:54:46 -0700

    updated vcf diffs

--- scripts/fetch_vcf.sh
+++ scripts/fetch_vcf.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-WORMVCF="3.1.0/VCF_WBcel235_95.vcf.gz"
-ZEBRAFISHVCF="3.1.0/VCF_GRCz11_95.vcf.gz"
-FLYVCF="3.1.0/VCF_R6_95.vcf.gz"
-MOUSEVCF="3.1.0/VCF_GRCm38_96.vcf.gz"
-RATVCF="3.1.0/VCF_Rnor60_76.vcf.gz"
+WORMVCF="3.1.1/VCF_WBcel235_17.vcf.gz"
+ZEBRAFISHVCF="3.1.1/VCF_GRCz11_17.vcf.gz"
+FLYVCF="3.1.1/VCF_R6_17.vcf.gz"
+MOUSEVCF="3.1.1/VCF_GRCm38_17.vcf.gz"
+RATVCF="3.1.1/VCF_Rnor60_18.vcf.gz"
 
 rm -rf ../apollo/data/worm/jbrowse
 mkdir ../apollo/data/worm/jbrowse
